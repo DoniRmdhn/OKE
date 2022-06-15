04246fc9edca5192890f329c64f538f19eafbea6
DoniRamadhan <95742544+Donigmg@users.noreply.github.com>
2022-06-15 21:06:30 +0700

    Update skip.py

--- YukkiMusic/plugins/admins/skip.py
+++ YukkiMusic/plugins/admins/skip.py
@@ -128,7 +128,7 @@ async def skip(cli, message: Message, _, chat_id):
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
-                f"[{title[:25]}](https://t.me/{app.username}?start=info_{vidid})", duration_min, user_name,
+                f"[{title[:25]}](https://t.me/{app.username}?start=info_{videoid})", check[0]["dur"], user,
             ),
             reply_markup=InlineKeyboardMarkup(button),
         )
@@ -156,7 +156,7 @@ async def skip(cli, message: Message, _, chat_id):
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
-                f"[{title[:25]}](https://t.me/{app.username}?start=info_{vidid})", duration_min, user_name,
+                f"[{title[:25]}](https://t.me/{app.username}?start=info_{videoid})", check[0]["dur"], user,
             ),
             reply_markup=InlineKeyboardMarkup(button),
         )
@@ -213,7 +213,7 @@ async def skip(cli, message: Message, _, chat_id):
             run = await message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
-                    f"[{title[:25]}](https://t.me/{app.username}?start=info_{vidid})", duration_min, user_name,
+                    f"[{title[:25]}](https://t.me/{app.username}?start=info_{videoid})", check[0]["dur"], user,
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
