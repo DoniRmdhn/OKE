eccc925945b5331ee7e08e649b7b4489506b9f61
DoniRamadhan <95742544+Donigmg@users.noreply.github.com>
2022-06-15 20:54:18 +0700

    Update skip.py

--- YukkiMusic/plugins/admins/skip.py
+++ YukkiMusic/plugins/admins/skip.py
@@ -128,7 +128,7 @@ async def skip(cli, message: Message, _, chat_id):
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
-                f"[{title[:25]}](https://t.me/{app.username}?start=info_{vidid})", duration_min, user,
+                f"[{title[:25]}](https://t.me/{app.username}?start=info_{vidid})", duration_min, user_name,
             ),
             reply_markup=InlineKeyboardMarkup(button),
         )
